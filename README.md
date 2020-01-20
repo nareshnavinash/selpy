8426d5e5527af240a6b3efade5158d9c47ac2cc6
Naresh Sekar <nareshnavinash@gmail.com>
2020-01-20 18:19:11 +0530

    Rename

---
+++ README.md
@@ -0,0 +1,6 @@
+# Se-Py Package
+
+python3 -m pip install --user --upgrade setuptools wheel
+
+python3 setup.py sdist bdist_wheel
+
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="SePy", # Replace with your own username
+    name="selpy", # Replace with your own username
     version="0.0.1",
     author="Naresh Sekar",
     author_email="nareshnavinash@gmail.com",
